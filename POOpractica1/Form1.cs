afe8c10adabe633b3bf4262f042fb690a8a56b06
Carlos Guillermo Reyes Severino <carlosguillermosrs@gmail.com>
2025-09-24 16:39:20 -0400

    Add project files.

---
+++ POOpractica1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Productos;
+
+namespace POOpractica1
+{
+    public partial class Form1 : Form
+    {
+
+        Celular Motorolla = new Celular();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_llamar_Click(object sender, EventArgs e)
+        {
+            Motorolla.Llamar();
+            //Motorolla.marca = "Samsung";
+            Motorolla.Llamar();
+        }
+    }
+}
---
+++ POOpractica1/Productos/Celular.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    public class Celular
+    {
+        private string marca = "Apple";
+        private string color;
+        private string tipo;
+
+        public void Llamar()
+        {
+
+            System.Windows.Forms.MessageBox.Show("Tu Movil es un: " + marca);
+
+        }
+
+        public void Mensaje() 
+        {
+
+        }
+
+    }
+}
